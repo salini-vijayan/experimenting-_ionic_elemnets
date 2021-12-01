cbe16e30784927066061a77460966e393452c7fc
Salini Vijayan <amsalinivijayan@gmail.com>
2021-12-01 12:50:00 +0530

    added total expenses

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesLists = document.querySelector("#expenses-list");
+const totalExpenseOutput = document.querySelector('#total-expense');
+
+let totalExpense = 0;
+
+const clear = () => {
+  reasonInput.value = '';
+  amountInput.value = '';
+};
 
 // Adding event listener
-
 confirmBtn.addEventListener("click", () => {
   const enteredReason = reasonInput.value;
   const enteredAmount = amountInput.value;
@@ -24,5 +31,14 @@ confirmBtn.addEventListener("click", () => {
   const newItem = document.createElement('ion-item');
   newItem.textContent = enteredReason + ' $' + enteredAmount; 
   expensesLists.appendChild(newItem);
+  totalExpense += +enteredAmount;
+  totalExpenseOutput.textContent = totalExpense;
+  clear();
 
 });
+
+
+cancelBtn.addEventListener('click',clear);
+/* clear without parantheses becoz i don't want to execute it immedialtely.
+Instead just pass a reference of the function to the eventlistener,
+so that this will be executed dynamically when this click occurs. */
